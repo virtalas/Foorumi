d3606eda1177b4f579a616807d1fb0086bdf58a1
virtalas <virtalas@users.noreply.github.com>
2015-12-10 16:23:05 +0200

    topics implemented in app

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -1,3 +1,2 @@
 FoorumApp.controller('ShowMessageController', function($scope, $routeParams, Api){
-  // Toteuta kontrolleri tähän
 });
--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,11 @@
-FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
+FoorumApp.controller('TopicsListController', function ($scope, $location, Api) {
+    Api.getTopics().success(function (topics) {
+        $scope.topics = topics;
+    });
+    
+    $scope.addTopic = function () {
+        Api.addTopic($scope.newTopic).success(function (topic) {
+            $location.path("/topics/" + topic.id);
+        });
+    }
 });
