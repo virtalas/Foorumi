e4383bd55bcad5086884e67d7de56f30395c16e1
virtalas <virtalas@users.noreply.github.com>
2015-12-10 18:33:01 +0200

    messages and replies implemented in app

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -1,2 +1,13 @@
-FoorumApp.controller('ShowMessageController', function($scope, $routeParams, Api){
+FoorumApp.controller('ShowMessageController', function ($scope, $routeParams, Api) {
+    Api.getMessage($routeParams.id).success(function (message) {
+        $scope.message = message;
+        console.log($scope.message);
+    });
+    
+    $scope.addReply = function () {
+        Api.addReply($scope.newReply, $scope.message.id).success(function (reply) {
+            $scope.message.Replies.push(reply);
+            $scope.newReply.content = "";
+        })
+    }
 });
--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -2,4 +2,10 @@ FoorumApp.controller('ShowTopicController', function ($scope, $routeParams, $loc
     Api.getTopic($routeParams.id).success(function (topic) {
         $scope.topic = topic;
     });
+    
+    $scope.addMessage = function () {
+        Api.addMessage($scope.newMessage, $scope.topic.id).success(function (message) {
+            $location.path("/messages/" + message.id);
+        });
+    }
 });
